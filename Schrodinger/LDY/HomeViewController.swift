33e20ec9fe04561b0d6bc188e90d4b8076633cb6
iDoyoung <ido.plum@gmail.com>
2021-07-30 15:41:29 +0900

    Implement Home View

--- Schrodinger/LDY/HomeViewController.swift
+++ Schrodinger/LDY/HomeViewController.swift
@@ -9,21 +9,63 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var firstSectionTitle: UILabel!
+    @IBOutlet weak var secondSectionTitle: UILabel!
+    @IBOutlet weak var thirdSectionTitle: UILabel!
+    @IBOutlet weak var fourthSectionTitle: UILabel!
+    @IBOutlet weak var checkTodayButton: UIButton!
+    @IBOutlet weak var expiredItemCollectionView: UICollectionView!
+    @IBOutlet weak var upcomingExpireCollectionView: UICollectionView!
+    
+    func applyLabel() {
+        firstSectionTitle.text = "Today expired item..".localized
+        secondSectionTitle.text = "Expired item".localized
+        thirdSectionTitle.text = "Upcoming expire".localized
+        fourthSectionTitle.text = "My History".localized
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func applyButton() {
+        checkTodayButton.setTitle("Check".localized, for: .normal)
+        checkTodayButton.addTarget(self, action: #selector(showTodayExpiredList), for: .touchUpInside)
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applyLabel()
+        expiredItemCollectionView.delegate = self
+        expiredItemCollectionView.dataSource = self
+        upcomingExpireCollectionView.delegate = self
+        upcomingExpireCollectionView.dataSource = self
+       
+    }
+    
+    @objc func showTodayExpiredList() {
+        //TODO: Apply push today expired list
+        
+    }
+   
+}
 
+//MARK: MOVE TO DETAIL
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+}
+
+class CellOfHomeView: UICollectionViewCell {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    
 }
