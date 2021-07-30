cc6a5a93daaa16704f063357d92d6b8212ec25a0
iDoyoung <ido.plum@gmail.com>
2021-07-30 19:26:32 +0900

    Implement Calendar View

--- Schrodinger/LDY/WorldViewController.swift
+++ Schrodinger/LDY/WorldViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class WorldViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
+
+}
+
+extension WorldViewController: UITableViewDelegate {
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension WorldViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "") as? CellOfWorldRanking else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    
+}
+class CellOfWorldRanking: UITableViewCell {
+    
 }
