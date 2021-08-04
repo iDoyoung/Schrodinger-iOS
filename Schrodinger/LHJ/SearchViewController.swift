12629763ab58409f394d8e2cde15dbd7ad03517a
hennie124 <81665973+hennie124@users.noreply.github.com>
2021-08-04 15:18:24 +0900

    image column 이용
    schrodinger 스키마의  product table 안에 있는 image column 을 이용해 화면에서
    image가 보이게 하기

--- Schrodinger/LHJ/SearchViewController.swift
+++ Schrodinger/LHJ/SearchViewController.swift
@@ -20,6 +20,12 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! SearchTableViewCell
         let item:DBSearchModel = feedItem[indexPath.row] as! DBSearchModel
         print(4)
+        let url = URL(string: "http://192.168.2.101:8080/schrodinger/images/\(item.image!)")
+        print("http://192.168.2.101:8080/schrodinger/images/\(item.image!)")
+        let data = try? Data(contentsOf: url!)
+        cell.ImgView.image = (UIImage(data: data!))
+        
+        print("사진 : \(item.image!)")
         
         cell.NameTitle?.text = "물품명 : \(item.name!)"
         cell.Date?.text = "유통기간 : \(item.expirationDate!)"
@@ -40,45 +46,51 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         
         listTableView.delegate = self
         listTableView.dataSource = self
+        listTableView.rowHeight = 60
         print(1)
         // Do any additional setup after loading the view.
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        let queryModel = SearchQueryModel()
-//        queryModel.delegate = self
-//        queryModel.downloadItems()
-//        print(2)
-//    }
+
     
     @IBAction func SegmentControl(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             let queryModel = SubmitQueryModel()
             queryModel.delegate = self
-            queryModel.downloadItem(name:SearchBar.text!)
+            queryModel.downloadItem(name: SearchBar.text!)
             print(2)
         }else if sender.selectedSegmentIndex == 1 {
             let queryModel = UseQueryModel()
             queryModel.delegate = self
-            queryModel.downloadItemed(name:SearchBar.text!)
+            queryModel.downloadItemed(name: SearchBar.text!)
             print(2)
         }else if sender.selectedSegmentIndex == 2 {
             let queryModel = SearchQueryModel()
             queryModel.delegate = self
-            queryModel.downloadItems(name:SearchBar.text!)
+            queryModel.downloadItems(name: SearchBar.text!)
             print(2)
         }
     }
     
     /*
     // MARK: - Navigation
-
+     //지금 합쳐지지 않아서 막아놓음 --> 테이블 셀 중 하나를 누르면 디테일 뷰로 해당 셀의 정보를 부르게 하기!!!!
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "sgDetail"{
+     let cell = sender as! UITableViewCell
+     let indexPath = self.listTableView.indexPath(for: cell)
+     let detailView = segue.destination as! DetailViewController
+     
+     let item:DBSearchModel = feedItem[indexPath!.row] as! DBSearchModel
+     
+     detailView.receivePno(pno: item.pno!)
+     print(8)
+     }
     }
     */
+  
+    
 
 }
 extension SearchViewController:UseQueryModelProtocol{
